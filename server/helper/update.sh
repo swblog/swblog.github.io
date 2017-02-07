bfff9bcd9c90993e8aa85f0bf0fa2e0dac933d26
liquidliang <liquidliang@tencet.com>
2017-02-07 12:46:49 +0800

    [update] article

--- server/helper/update.sh
+++ server/helper/update.sh
@@ -1,1 +1,1 @@
-git add . && git commit -am '[update] article' && git push origin master
+ git pull origin master && git add . && git commit -am '[update] article' && git push origin master
--- server/update-index.js
+++ server/update-index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const getIndex = require('./util/markdown-json-index');
+const {exec, execFile} = require('child_process');
 
 fs.writeFile('./json/article.json',
   JSON.stringify(getIndex('blog'), null, 1),
@@ -8,3 +9,59 @@ fs.writeFile('./json/article.json',
     console.log('It\'s saved to article.json!');
   }
 );
+
+
+exec("git pull origin master", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
+
+exec("git pull origin master", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
+
+exec("git add .", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
+exec("git commit -am '[update] article", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
+exec("git push origin master", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
+//
+//
+//  // && git add . && git commit -am '[update] article' && git push origin master
+
+
+// execFile('./helper/update.sh', function (error, stdout, stderr) {
+//     console.log('stdout: ' + stdout);
+//     console.log('stderr: ' + stderr);
+//     if (error !== null) {
+//       console.log('exec error: ' + error);
+//     }
+// });
