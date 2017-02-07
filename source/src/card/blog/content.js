d59f3e06bb30856ff9bf9dceb948b4663c18fbf9
liquidliang <liquidliang@tencet.com>
2017-02-07 22:00:43 +0800

    [update] article

--- source/src/card/blog/content.js
+++ source/src/card/blog/content.js
@@ -17,9 +17,9 @@ module.exports = function(option) {
      '<ul class="pager">' +
      '  <li class="previous"><a data-on="?m=back">← 返回</a></li>' +
       ' <li><a target="_blank" href="https://github.com/'+
-      '<%=username%>/<%=username%>.github.io/issues?utf8=%E2%9C%93&q=<%=decodeURIComponent(isLocalhost ? "localhost测试评论" : obj.time)%>">查看评论</a></li>' +
+      '<%=username%>/<%=username%>.github.io/issues?utf8=%E2%9C%93&q=<%=decodeURIComponent(isLocalhost ? "localhost测试评论" : obj.title)%>">查看评论</a></li>' +
       ' <li class="next"><a target="_blank" href="https://github.com/'+
-      '<%=username%>/<%=username%>.github.io/issues/new?title=<%=decodeURIComponent(isLocalhost ? "localhost测试评论" : obj.time)%>">去评论 &rarr;</a></li>' +
+      '<%=username%>/<%=username%>.github.io/issues/new?title=<%=decodeURIComponent(isLocalhost ? "localhost测试评论" : obj.title)%>">去评论 &rarr;</a></li>' +
      '</ul>'
  }, option);
 };
