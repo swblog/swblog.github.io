bc31c094c2ecd80d48b6f70f5e206cb6754b0a5d
liquidliang <liquidliang@tencet.com>
2017-02-07 21:31:27 +0800

    [update] article

--- server/util/markdown-json-index.js
+++ server/util/markdown-json-index.js
@@ -30,16 +30,14 @@ const reviseTime = function(list, callback){
   iterator(list, function(item, next, arr){
     console.log(item.path);
     myGit.log(['-1', item.path], (err, result) => {
-      let originItem = arr.filter(o=>o.path==item.path)[0] || item;
+      let originItem = list.filter(o=>o.path==item.path)[0] || item;
       let logLine = result && result.latest;
       if(logLine){
-        console.log('logLine', logLine);
+        //console.log('logLine', logLine);
         originItem.mtime = new Date(logLine.date).getTime();
         originItem.message = logLine.message;
-      }else{
-        console.log('logLine null');
       }
-      console.log('originItem', originItem);
+      //console.log('originItem', originItem);
 
       if(originItem.child && originItem.child.length){
         reviseTime(originItem.child, next);
@@ -48,7 +46,6 @@ const reviseTime = function(list, callback){
       }
 
       if(arr.length===0){
-        console.log('callback');
         callback(list);
       }
     });
