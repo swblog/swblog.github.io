a82f120d8c9fe8c46a085be03cf80b1d7c4f8500
liquidliang <liquidliang@tencet.com>
2017-02-07 12:51:30 +0800

    [update] article

--- server/helper/update.sh
+++
@@ -1,1 +0,0 @@
- git pull origin master && git add . && git commit -am '[update] article' && git push origin master
--- server/update-index.js
+++ server/update-index.js
@@ -11,7 +11,9 @@ fs.writeFile('./json/article.json',
 );
 
 
-exec('git commit -am "[update] article"', (error, stdout, stderr) => {
+
+
+exec("git pull origin master", (error, stdout, stderr) => {
   if (error) {
     console.error(`exec error: ${error}`);
     return;
@@ -20,48 +22,27 @@ exec('git commit -am "[update] article"', (error, stdout, stderr) => {
   console.log(`stderr: ${stderr}`);
 });
 
-// exec("git pull origin master", (error, stdout, stderr) => {
-//   if (error) {
-//     console.error(`exec error: ${error}`);
-//     return;
-//   }
-//   console.log(`stdout: ${stdout}`);
-//   console.log(`stderr: ${stderr}`);
-// });
-
-// exec("git add .", (error, stdout, stderr) => {
-//   if (error) {
-//     console.error(`exec error: ${error}`);
-//     return;
-//   }
-//   console.log(`stdout: ${stdout}`);
-//   console.log(`stderr: ${stderr}`);
-// });
-// exec("git commit -am '[update] article", (error, stdout, stderr) => {
-//   if (error) {
-//     console.error(`exec error: ${error}`);
-//     return;
-//   }
-//   console.log(`stdout: ${stdout}`);
-//   console.log(`stderr: ${stderr}`);
-// });
-// exec("git push origin master", (error, stdout, stderr) => {
-//   if (error) {
-//     console.error(`exec error: ${error}`);
-//     return;
-//   }
-//   console.log(`stdout: ${stdout}`);
-//   console.log(`stderr: ${stderr}`);
-// });
-//
-//
-//  // && git add . && git commit -am '[update] article' && git push origin master
-
-
-// execFile('./helper/update.sh', function (error, stdout, stderr) {
-//     console.log('stdout: ' + stdout);
-//     console.log('stderr: ' + stderr);
-//     if (error !== null) {
-//       console.log('exec error: ' + error);
-//     }
-// });
+exec("git add .", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
+exec('git commit -am "[update] article"', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
+exec("git push origin master", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
